d7aed18deddb89fa4870c87c2ce5152236f255b4
Neil <neiltz711@gmail.com>
2019-02-25 15:55:39 -0500

    Create title.jsx

---
+++ app/title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Title extends React.Component{
+  render(){
+    return(
+      <div className="text-center">
+        <h1>Exercise Tracker</h1>
+        <p>Create a user and track your exercises!</p>
+      </div>
+    );
+  }
+}
+
+export default Title;
